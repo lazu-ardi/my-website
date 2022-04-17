6778f2d7f2c39f7c1f50a3a08654c0b749f2b535
Lazuardi <lazuardi_imani@pm.me>
2022-04-17 22:56:34 +0700

    feat: implement dark mode switch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ],
+    },
     extend: {},
   },
   plugins: [],
