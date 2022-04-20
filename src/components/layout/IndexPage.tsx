ff3e23258d4101003ea5e410751d444c28e8fe01
Lazuardi <lazuardi_imani@pm.me>
2022-04-20 23:51:17 +0700

    feat: implement head meta tags

---
+++ src/components/layout/IndexPage.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Lazuardi</title>
+        <meta name="description" content="Lazuardi Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default IndexPage;
