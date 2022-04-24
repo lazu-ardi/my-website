7d54196224e65b11b8ddfcb72ff924a3af271170
Lazuardi <lazuardi_imani@pm.me>
2022-04-24 17:08:13 +0700

    feat: implement index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next';
-import IndexPage from '../components/IndexPage';
+import IndexAbout from '../components/layout/IndexAbout';
+import IndexCallout from '../components/layout/IndexCallout';
+import IndexHeader from '../components/layout/IndexHeader';
+import IndexProjects from '../components/layout/IndexProjects';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <IndexPage />
+    <div className="flex flex-col items-center justify-center py-2 mx-auto mt-4 antialiased max-w-fit">
+      <IndexHeader />
+      <IndexCallout />
+      <IndexAbout />
+      <IndexProjects />
     </div>
   );
 };
