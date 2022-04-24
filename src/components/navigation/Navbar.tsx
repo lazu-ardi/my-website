f1d5585fb9adae07f3fd0f62525a70f6f57b69c4
Lazuardi <lazuardi_imani@pm.me>
2022-04-25 02:21:27 +0700

    feat: change color and make it sticky in mobile view

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -4,17 +4,18 @@ import ThemeSwitch from '../layout/ThemeSwitch';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-zinc-800">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <Link href="/">
-          <a className="flex items-center">
-            <span className="text-slate-900 self-center text-2xl font-semibold whitespace-nowrap hover:text-emerald-400 hover:dark:text-cyan-400 dark:text-slate-50">
-              Lazuardi
-            </span>
-          </a>
-        </Link>
-        <div className="flex flex-row gap-4">
-          {/* <Link href="/">
+    <div className="sticky top-0 z-20 lg:static">
+      <nav className="bg-slate-50 px-2 sm:px-4 py-2.5 dark:bg-zinc-800">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <Link href="/">
+            <a className="flex items-center">
+              <span className="text-slate-900 self-center text-2xl font-semibold whitespace-nowrap hover:text-emerald-400 hover:dark:text-cyan-400 dark:text-slate-50">
+                Lazuardi
+              </span>
+            </a>
+          </Link>
+          <div className="flex flex-row gap-4">
+            {/* <Link href="/">
             <a className="text-slate-900 self-center text-lg font-semibold whitespace-nowrap hover:text-emerald-400 hover:dark:text-cyan-400 dark:text-slate-50">
               About
             </a>
@@ -24,15 +25,16 @@ const Navbar = () => {
               Project
             </a>
           </Link> */}
-          <Link href="https://drive.google.com/file/d/164ypBN4i-wGsVzM2Jpf5aTJJw4JQ2sWg/view?usp=sharing">
-            <a className="text-slate-900 self-center text-lg font-semibold whitespace-nowrap hover:text-emerald-400 hover:dark:text-cyan-400 dark:text-slate-50">
-              Resume
-            </a>
-          </Link>
-          <ThemeSwitch />
+            <Link href="https://drive.google.com/file/d/164ypBN4i-wGsVzM2Jpf5aTJJw4JQ2sWg/view?usp=sharing">
+              <a className="text-slate-900 self-center text-lg font-semibold whitespace-nowrap hover:text-emerald-400 hover:dark:text-cyan-400 dark:text-slate-50">
+                Resume
+              </a>
+            </Link>
+            <ThemeSwitch />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
